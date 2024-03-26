4434e489f52713ec7d3ee2abbbf78fdf71fb0e82
YasminFaraj <yasminfaraj02@gmail.com>
2024-03-26 16:28:44 -0300

    Código Ex1 completo
    Aqui terminei de colocar o resto do código dentro do Main, adicionando Switch para selecionar as opções da calculadora. E na Calculadora adicionei o restante das operações necessárias.

--- Ex1/Ex1/src/Calculadora.java
+++ Ex1/Ex1/src/Calculadora.java
@@ -4,4 +4,21 @@ public class Calculadora {
         return num1 + num2;   
     }
 
+    public static float sub(float num1, float num2){
+        return num1 - num2;   
+    }
+
+    public static float mult(float num1, float num2){
+        return num1 * num2;   
+    }
+
+    public static float div(float num1, float num2){
+        if(num1 == 0 || num2 == 0){
+            System.out.println("Impossível dividir por 0");
+            return 0;
+        }
+        return num1 / num2;   
+    }
+
+
 }
--- Ex1/Ex1/src/Main.java
+++ Ex1/Ex1/src/Main.java
@@ -9,7 +9,26 @@ public class Main {
 
         System.out.print("Informe o número 2: ");
         float num2 = Float.parseFloat(leitor.nextLine());
+
+        System.out.print("Informe qual operação será feita: ");
+        int opcao = Integer.parseInt(leitor.nextLine());
         
-        System.out.println("Soma: " + Calculadora.adicao(num1, num2));
+        switch(opcao){
+            case 1:
+            System.out.println("Soma: " + Calculadora.adicao(num1, num2));
+            break;
+
+            case 2:
+            System.out.println("Subtração: " + Calculadora.sub(num1, num2));
+            break;
+
+            case 3:
+            System.out.println("Multiplicação: " + Calculadora.mult(num1, num2));
+            break;
+
+            case 4:
+            System.out.println("Divisão: " + Calculadora.div(num1, num2));
+            break;
+        }
     }
 }
