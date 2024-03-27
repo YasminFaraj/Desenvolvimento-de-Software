e4e5e37948b5cbb179ca37fda1b8fd08cd266e22
YasminFaraj <yasminfaraj02@gmail.com>
2024-03-27 17:14:37 -0300

    Inicio

---
+++ Ex3/EX3/src/ContaBancaria.java
@@ -0,0 +1,12 @@
+public class ContaBancaria {
+
+    private int numeroConta;
+    private String nomeTitular;
+    private float saldo;
+
+    public ContaBancaria(int numeroConta, String nomeTitular){
+        this.numeroConta = numeroConta;
+        this.nomeTitular = nomeTitular;
+        this.saldo = 0;
+    }
+}
---
+++ Ex3/EX3/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        ContaBancaria conta = new ContaBancaria(1, "Yasmin Faraj");
+
+        System.out.println(conta);
+
+    }
+}
