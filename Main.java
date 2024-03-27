8b7f860878978947ae699ae1c9722caf9bbf5c4d
YasminFaraj <164505252+YasminFaraj@users.noreply.github.com>
2024-03-27 17:04:07 -0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args){
-        ContaBancaria conta = new ContaBancaria(1, "Yasmin Faraj");
-
-        System.out.println(conta);
-
-    }
-}
