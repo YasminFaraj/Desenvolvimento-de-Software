b92d4a29c65039a83c37a594ea25a4cfd7b3aed6
YasminFaraj <yasminfaraj02@gmail.com>
2024-03-27 17:38:38 -0300

    Inicio
    iniciando uma conta com parametros

--- Ex3/EX3/src/Main.java
+++ Ex3/EX3/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args){
         ContaBancaria conta = new ContaBancaria(1, "Yasmin Faraj");
 
+        
         System.out.println(conta);
     }
 }
