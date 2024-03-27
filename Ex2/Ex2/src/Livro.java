bb0b4cef32ce7d80c47481c140a526f14f9555f1
YasminFaraj <yasminfaraj02@gmail.com>
2024-03-27 16:24:59 -0300

    Livro.java
    Inicio da Livro.java

---
+++ Ex2/Ex2/src/Livro.java
@@ -0,0 +1,13 @@
+public class Livro {
+    String titulo;
+    String autor;
+    int ano;
+
+    Livro(String tt, String aut, int ano) {
+        titulo = tt;
+        autor = aut;
+        this.ano = ano;
+    }
+
+    
+}
