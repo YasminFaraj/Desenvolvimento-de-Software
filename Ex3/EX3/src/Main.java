7ce0f98a2f99d0f9e759711c017c9f78a6353b3c
YasminFaraj <yasminfaraj02@gmail.com>
2024-03-29 13:29:33 -0300

    adicionei uma nova conta

--- Ex3/EX3/src/Main.java
+++ Ex3/EX3/src/Main.java
@@ -6,6 +6,12 @@ public class Main {
         conta.setSacar(350.3f);//esse f serve pro compilador entender que é um número float
         conta.setDepositar(100);
 
+        ContaBancaria conta2 = new ContaBancaria(2, "Jonatas Moura");
+
+        conta2.setDepositar(3000);
+        conta2.setSacar(2.9f);
+        
+        System.out.println(conta2.imprimirInfo());
         System.out.println(conta.imprimirInfo());
     }
 }
