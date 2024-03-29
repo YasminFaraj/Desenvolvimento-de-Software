855183dee240e1713e8369887ab8a67c1174ac86
YasminFaraj <yasminfaraj02@gmail.com>
2024-03-29 12:21:39 -0300

    andamento
    adicionei os saques e depósitos, juntamente com as estruturas sacar e depositar, além da getSaldo e exibirInfo().

--- Ex3/EX3/src/ContaBancaria.java
+++ Ex3/EX3/src/ContaBancaria.java
@@ -8,4 +8,31 @@ public class ContaBancaria {
         this.nomeTitular = nomeTitular;
         this.saldo = 0;
     }
+
+    public void depositar(float valor){
+        if(valor > 0){
+            this.saldo += valor;
+            System.out.println("Depósito realizado!");
+        }
+        else{
+            System.out.println("Você só pode depositar valores acima de 0 reais.");
+        }
+    }
+
+    public void sacar(float valor){
+        if(valor > 0 && valor <= saldo){
+            this.saldo -= valor;
+            System.out.println("Saque no valor " + valor + " realizado.");
+        }else{
+            System.out.println("Você só pode sacar valores acima de 0 reais.");
+        }
+    }
+
+    public float getSaldo(){
+        return this.saldo;
+    }
+
+    public String imprimirInfo(){
+        return "Número da Conta: " + this.numeroConta + ", Titular: " + this.nomeTitular + ", Saldo: " + this.getSaldo();
+    }
 }
--- Ex3/EX3/src/Main.java
+++ Ex3/EX3/src/Main.java
@@ -3,6 +3,11 @@ public class Main {
     public static void main(String[] args){
         ContaBancaria conta = new ContaBancaria(1, "Yasmin Faraj");
 
-        System.out.println(conta);
+        conta.depositar(1000);
+        conta.sacar(350.3f);//esse f serve pro compilador entender que é um número float
+        conta.depositar(350.3f);
+        conta.depositar(100);
+
+        System.out.println(conta.imprimirInfo());
     }
 }
