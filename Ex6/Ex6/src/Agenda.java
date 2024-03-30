49141ac131eb7b2a6105bad96ba7c8f11cba0fca
YasminFaraj <yasminfaraj02@gmail.com>
2024-03-30 19:02:13 -0300

    completo

---
+++ Ex6/Ex6/src/Agenda.java
@@ -0,0 +1,59 @@
+import java.util.List;
+import java.util.Scanner;
+import java.util.Iterator;
+
+public class Agenda {
+    private Scanner leitor = new Scanner(System.in);
+
+    public void adicionarContato(List<Contato> contatos) {
+        System.out.println("Informe o nome: ");
+        String nome = leitor.nextLine();
+
+        System.out.println("Informe o número: ");
+        String numero = leitor.nextLine();
+
+        Contato novoContato = new Contato(nome, numero);
+        contatos.add(novoContato);
+
+        System.out.println("Contato adicionado com sucesso.");
+    }
+
+    public void removerContato(List<Contato> contatos) {
+        System.out.println("Informe o nome do contato que deseja remover: ");
+        String nome = leitor.nextLine();
+    
+        boolean removido = false;
+    
+        for (int i = 0; i < contatos.size(); i++) {
+            Contato contato = contatos.get(i);
+            if (contato.getNome().equalsIgnoreCase(nome)) {
+                contatos.remove(i);
+                removido = true;
+                System.out.println("Contato removido com sucesso.");
+                break;
+            }
+        }
+    
+        if (!removido) {
+            System.out.println("Contato não encontrado.");
+        }
+    }
+    
+
+    public void buscarContato(List<Contato> contatos) {
+        System.out.println("Informe o nome do contato que deseja buscar: ");
+        String nome = leitor.nextLine();
+
+        boolean encontrado = false;
+
+        for (Contato contato : contatos) {
+            if (contato.getNome().equalsIgnoreCase(nome)) {
+                System.out.println("Nome: " + contato.getNome() + ", Número: " + contato.getTelefone());
+                encontrado = true;
+            }
+        }
+        if (!encontrado) {
+            System.out.println("Contato não encontrado.");
+        }
+    }
+}
---
+++ Ex6/Ex6/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+
+        Agenda agenda = new Agenda();
+        List<Contato> contatos = new ArrayList<Contato>();
+
+        int opcao;
+
+        do {
+            System.out.println("Selecione uma opção: ");
+            System.out.println("1. Adicionar contato.");
+            System.out.println("2. Remover contato.");
+            System.out.println("3. Buscar contato.");
+            System.out.println("4. Sair.");
+
+            opcao = leitor.nextInt();
+
+            leitor.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    agenda.adicionarContato(contatos);
+                    break;
+                case 2:
+                    agenda.removerContato(contatos);
+                    break;
+                case 3:
+                    agenda.buscarContato(contatos);
+                    break;
+                case 4:
+                    System.out.println("Programa encerrado.");
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+            }
+        } while (opcao != 4);
+    }
+}
+
